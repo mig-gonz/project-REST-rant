27897c77abfa1aa76e591bf239708029dd615f1e
Miguel <itsmig@proton.me>
2023-02-16 14:04:19 -0800

    Delete functionality implemented

--- views/default.jsx
+++ views/default.jsx
@@ -9,7 +9,7 @@ function Def(html) {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         />
-        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="/style.css" />
         <title>Title</title>
       </head>
       <body>
