a6b53d691402c896cb423c2edd3c8006767c2824
Miguel <itsmig@proton.me>
2023-02-15 17:56:14 -0800

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,24 @@
 const router = require("express").Router();
+const places = require("../models/places.js");
 
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/REST-rant-one.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/REST-rant-two.jpg",
-    },
-  ];
-
   res.render("places/index", { places });
+});
+
+router.post("/", (req, res) => {
+  // console.log(req.body);
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "public/images/blue-eyed-pup.jpg";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
 router.get("/new", (req, res) => {
--- index.js
+++ index.js
@@ -8,14 +8,13 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
+app.use("/places", require("./controllers/places"));
+app.use(express.urlencoded({ extended: true }));
 
 // Controllers & Routes
-app.use("/places", require("./controllers/places"));
-
 app.get("/", (req, res) => {
   res.render("home");
 });
-
 app.get("*", (req, res) => {
   res.render("error404");
 });
