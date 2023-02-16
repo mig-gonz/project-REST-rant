351a3b1e95808abdeef7bce865866de2898d2e1e
Miguel <itsmig@proton.me>
2023-02-16 15:34:10 -0800

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -50,4 +50,16 @@ router.delete("/:id", (req, res) => {
   }
 });
 
+// edit
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id] });
+  }
+});
+
 module.exports = router;
