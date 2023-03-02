96d6f24574979d14cb3b9afe59dc80f52f25c4c7
Miguel <itsmig@proton.me>
2023-03-01 18:33:24 -0800

    refactored code. added rants/comment

--- controllers/places.js
+++ controllers/places.js
@@ -33,7 +33,9 @@ router.get("/new", (req, res) => {
 //  show
 router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
+    .populate("comments")
     .then((place) => {
+      console.log(place.comments);
       res.render("places/show", { place });
     })
     .catch((err) => {
@@ -42,9 +44,32 @@ router.get("/:id", (req, res) => {
     });
 });
 
+//  put
+router.put("/:id", (req, res) => {
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect(`/places/${req.params.id}`);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
+
+// EDIT
+router.get("/:id/edit", (req, res) => {
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/edit", { place });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
+});
+
 // delete route
 router.delete("/:id", (req, res) => {
-  db.Place.deleteOne({ id: req.params.id })
+  db.Place.findByIdAndDelete(req.params.id)
     .then(() => res.redirect("/places"))
     .catch((err) => {
       console.log(err);
@@ -52,14 +77,26 @@ router.delete("/:id", (req, res) => {
     });
 });
 
-//  put
-router.put("/:id", (req, res) => {
-  res.send("PUT /places/:id stub");
-});
-
-// edit
-router.get("/:id/edit", (req, res) => {
-  res.send("GET edit form stub");
+// comment
+router.post("/:id/comment", (req, res) => {
+  console.log(req.body);
+  req.body.rant = req.body.rant ? true : false;
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place.save().then(() => {
+            res.redirect(`/places/${req.params.id}`);
+          });
+        })
+        .catch((err) => {
+          res.render("error404");
+        });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
 });
 
 module.exports = router;
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,36 +2,95 @@ const React = require("react");
 const Def = require("../default");
 
 function show(data) {
+  console.log(data);
+  let comments = <h3>No comments yet!</h3>;
+  let rating = <h3>Not yet rated</h3>;
+  if (data.place.comments.length) {
+    comments = data.place.comments.map((c) => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? "Rant! ðŸ˜¡" : "Rave! ðŸ˜»"}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      );
+    });
+  }
   return (
     <Def>
       <main>
         <div className="row">
           <div className="col-sm-6">
-            <h1>{data.place.name}</h1>
             <img src={data.place.pic} alt={data.place.name} />
             <h3>
-              Located in {data.place.city}, {data.place.state}{" "}
-              {data.place.cuisines}
+              Located in {data.place.city}, {data.place.state}
             </h3>
           </div>
           <div className="col-sm-6">
-            <h2>Rating</h2>
-            <p>Not yet rated.</p>
+            <h1>{data.place.name}</h1>
+            <h2>Rating:</h2>
+            {rating}
             <h2>Description</h2>
             <h3>{data.place.showEstablished()}</h3>
             <h4>Serving {data.place.cuisines}</h4>
-            <h2>Comments</h2>
-            <p>No comments yet!</p>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            <a
+              href={`/places/${data.place._id}/edit`}
+              className="btn btn-warning"
+            >
               Edit
             </a>
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+            <form
+              method="POST"
+              action={`/places/${data.place._id}?_method=DELETE`}
+            >
               <button type="submit" className="btn btn-danger">
                 Delete
               </button>
             </form>
           </div>
         </div>
+        <hr />
+        <h2>Comments</h2>
+        <form method="POST" action={`/places/${data.place._id}/comment`}>
+          <div className="form-group">
+            <label htmlFor="author">Author: </label>
+            <input className="form-control" type="text" name="author" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="content">comment:</label>
+            <input
+              type="text-area"
+              className="form-control"
+              id="content"
+              name="content"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="stars">rating: (0-5)</label>
+            <input
+              type="text"
+              className="form-control"
+              id="stars"
+              name="stars"
+              step="0.5"
+              min="0"
+              max="5"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="rant">Rant </label>
+            <input type="checkbox" name="rant" value="rant" />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+        {comments}
       </main>
     </Def>
   );
